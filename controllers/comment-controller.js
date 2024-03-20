4b1c456474cdaaffea7851badc14712de185dadf
Suboch. S <sema.chel12@gmail.com>
2024-03-20 17:34:11 +0300

    CommentController/deleteComment

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -26,9 +26,20 @@ const CommentController = {
 		}
 	},
 	deleteComment: async (req, res) => {
+		const { id } = req.params;
+		const userId = req.user.userId;
 		try {
+			const comment = await prisma.comment.findUnique({ where: { id } });
+			if (!comment) {
+				return res.status(404).json({ error: 'Комментарий не найден' });
+			}
+			if (comment.userId !== userId) {
+				return res.status(403).json({ error: 'Нет доступа' });
+			}
+			await prisma.comment.delete({ where: { id } });
+			res.json(comment);
 		} catch (error) {
-			console.error('Delete comment error', error);
+			console.error('Error delete comment', error);
 			res.status(500).json({ error: 'Internal server error' });
 		}
 	},
