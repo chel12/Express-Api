8aa1d241344054663101a564280035fcb8b4e6ab
Suboch. S <sema.chel12@gmail.com>
2024-03-20 16:05:46 +0300

    PostController/getAllPosts

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -21,7 +21,38 @@ const PostController = {
 		}
 	},
 	getAllPosts: async (req, res) => {
+		const userId = req.user.userId;
 		try {
+			//верни все посты и включи в них лайки, автора, комментарии
+			const posts = await prisma.post.findMany({
+				include: {
+					likes: true,
+					author: true,
+					comments: true,
+				},
+				//отсортируй, по созданию, по убыванию
+				orderBy: {
+					createdAt: 'desc',
+				},
+			});
+			//текущий пользователь лайкнул пост или нет
+			const postWithLikeInfo = posts.map((post) => ({
+				//распаковывает
+				...post,
+				//добавляет likedByUser и в нем смотрит
+				/*
+				post.likes.some((like) => like.userId === userId): 
+				Метод some проверяет, удовлетворяет ли хотя бы один элемент массива 
+				(в данном случае, элементы массива likes в объекте post) 
+				определённому условию. В данном случае, он проверяет, есть ли хотя бы один "лайк", где userId совпадает с userId, 
+				который предположительно представляет текущего пользователя.
+				Таким образом, в результате каждый объект post в массиве posts будет скопирован, 
+				а к нему будет добавлено новое свойство likedByUser, которое будет равно true,
+				если текущий пользователь поставил "лайк" этому посту, и false в противном случае.
+				*/
+				likedByUser: post.likes.some((like) => like.userId === userId),
+			}));
+			res.json(postWithLikeInfo);
 		} catch (error) {
 			console.error('Get All Posts error', error);
 			res.status(500).json({ error: 'Internal server error' });
