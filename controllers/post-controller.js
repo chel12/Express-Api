a9e72a2eb380134f42ffe89914998318e233315a
Suboch. S <sema.chel12@gmail.com>
2024-03-20 15:43:41 +0300

    PostController/createPost

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,17 +1,46 @@
 const { prisma } = require('../prisma/prisma-client');
 const PostController = {
 	createPost: async (req, res) => {
-		res.send('createPost');
+		//создаём, остальное создатся динамически
+		const { content } = req.body;
+		//с мидл вар приходит и берем так
+		const authorId = req.user.userId;
+		if (!content) {
+			return res.status(400).json({ error: 'Все поля обязательны' });
+		} // это доступно после получения токена, поэтому на юзера нет смысла проверять
+
+		try {
+			//создание поста
+			const post = await prisma.post.create({
+				data: { content, authorId },
+			});
+			res.json(post);
+		} catch (error) {
+			console.error('Error Create Post', error);
+			res.status(500).json({ error: 'Internal server error' });
+		}
 	},
 	getAllPosts: async (req, res) => {
-		res.send('getAllPosts');
+		try {
+		} catch (error) {
+			console.error('Get All Posts error', error);
+			res.status(500).json({ error: 'Internal server error' });
+		}
 	},
 	getPostById: async (req, res) => {
-		res.send('getPostById');
+		try {
+		} catch (error) {
+			console.error('Get Post By Id', error);
+			res.status(500).json({ error: 'Internal server error' });
+		}
 	},
 	deletePost: async (req, res) => {
-		res.send('deletePost');
+		try {
+		} catch (error) {
+			console.error('delete post', error);
+			res.status(500).json({ error: 'Internal server error' });
+		}
 	},
 };
 
-module.exports = PostController ;
+module.exports = PostController;
